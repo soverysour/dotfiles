261b11aa8c8c502703885a05db9594524bb00f30
soverysour <sebi.f@tuta.io>
2025-07-08 20:40:45 +0300

    add hl-todo

--- init.el
+++ init.el
@@ -1,6 +1,18 @@
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (add-to-list 'custom-theme-load-path (expand-file-name "~/.emacs.d/themes/"))
 
+(use-package hl-todo
+  :hook (prog-mode . hl-todo-mode)
+  :config
+  (setq hl-todo-highlight-punctuation ":"
+        hl-todo-keyword-faces
+        `(("TODO"       warning bold)
+          ("FIXME"      error bold)
+          ("HACK"       font-lock-constant-face bold)
+          ("REVIEW"     font-lock-keyword-face bold)
+          ("NOTE"       success bold)
+          ("DEPRECATED" font-lock-doc-face bold))))
+
 (use-package markdown-mode
   :ensure t
   :mode ("\\.md\\'" . gfm-mode)
